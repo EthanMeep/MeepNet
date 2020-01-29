9889bd2363c4b32750b4c22b76c0ae4a896a7784
EthanMeep <ethan.calabritto@gmail.com>
2020-01-28 23:49:06 -0500

    Updated to V5

---
+++ configV5.py
@@ -0,0 +1,25 @@
+#email server
+server = ''
+
+#email server port
+port = 587
+
+#password to all emails
+clientpass = " "
+
+#your emails
+clients = [' ']
+
+supported_carriers = ['att', '@txt.att.net',
+		      'cellone', '@sms.cellonenation.net',
+		      'verizon', '@vtext.com',
+		      'alltel', '@message.alltel.com',
+		      'boost', '@myboostmobile.com',
+		      'cricket', '@sms.mycricket.com',
+		      'metropcs', '@mymetropcs.com',
+		      'sprint', '@messaging.sprintpcs.com',
+		      'nextel', '@page.nextel.com',
+		      'straighttalk', '@vtext.com',
+		      'tmobile', '@tmomail.net',
+		      'uscellular', '@email.uscc.net',
+		      'virgin', '@vmobl.com']
